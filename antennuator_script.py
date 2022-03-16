8e244719270cdc8a75e9ed36320866dece206516
Philip Buechler <philippbuechlerrz@gmail.com>
2022-03-16 11:06:17 +0100

    first commit
    add main script and readme.md

---
+++ antennuator_script.py
@@ -0,0 +1,171 @@
+import csv
+import serial
+from time import sleep
+import sys
+import getopt
+import serial.tools.list_ports
+
+global serialport
+serialport = ''
+
+def openserialport(serial_number):
+
+	try:
+		global serialport
+		serialport = serial.Serial(serial_number, baudrate=9600, timeout=0.0)
+		return
+
+	except SerialPortError:
+		print("Could not open serial port, use -h or --help")
+		return
+
+def csvloop(file):
+
+	global serialport
+
+	try:
+		with open(file) as patterncsv:
+			pattern_csv_object = csv.reader(patterncsv, delimiter=';')
+
+	except FileNotFound:
+		print("Could not find file, use -h or --help")
+		return
+
+
+#pattern_csv:
+#new way: dwell_time,chain1,chain2,chain3,chain4
+#duaration = input("enter time duaration in minutes: ") * 60
+#time_start = time.time()
+#while time.time() < time_start + duaration:
+	while true:
+		for row in pattern_csv_object:
+			count = 1
+			for  chell in row:
+				if count < 5:
+					antennuation = row[(count)]
+					command = "SET "+str((count))+" "+str(antennuation)
+					serialport.write((command).encode())
+					print(str(serialport.readline()).strip('\'b\\r\\n'))
+					print(str(serialport.readline()).strip('\'b\\r\\n'))
+				count += 1
+			sleep(int(row[0]/1000))
+
+def help():
+
+		print(
+"""usage:
+\t-h --help\t\t\t\tshows this help
+
+\t-p --port\t[serialport]\t\tpath to serial port the device is in
+\t\t\t\t\t\tthis option is required for every function except help
+
+\t-s --set_value\t[chain:antennuation]\tsets given chain to a given attennuation.
+\t\t\t\t\t\tavailable chains: 1 2 3 4
+\t\t\t\t\t\tantennuation needs to be between 0 and 95dB
+\t\t\t\t\t\tthe resolution is 0.25dB
+
+\t-t --csv_table\t[path to csv file]
+\t\t\t\t\t\tinstructs the antennuator to run an antennutation pattern
+\t\t\t\t\t\twhen using this option the script needs to be terminated manually
+\t\t\t\t\t\tsee in readme how the csv file needs to be filled
+
+\t-i --info\t\t\t\treads out the status of the device
+
+\t--portinfo\t\t\t\tshows list of connected serial devices
+
+
+""")
+		return
+
+def info():
+
+	global serialport
+
+	serialport.write(("info").encode())
+	output = serialport.readlines()
+	print("")
+	for line in output[1:]:
+		print (str(line).strip('\'b\\r\\n'))
+	serialport.close()
+	return
+
+def setvalue(anttenuation):
+
+	global serialport
+
+	par = anttenuation.split(":")
+	command = "SET "+str(par[0])+" "+str(par[1])
+	serialport.write((command).encode())
+	sleep(0.01)
+	print(str(serialport.readline()).strip('\'b\\r\\n'))
+	print(str(serialport.readline()).strip('\'b\\r\\n'))
+	serialport.close()
+	return
+
+def portinfo():
+
+	portlist = serial.tools.list_ports.comports()
+	for port in portlist:
+		print(port.description)
+
+def argumentcheck():
+
+	portopt = False
+	argv = sys.argv[1:]
+
+	try:
+		opts, args = getopt.getopt(argv,"dhp:s:t:i",["help","port=","set_value=","csv_table=","info","portinfo"])
+	except:
+		print("""incorrect input use -h or --help for a list of options""")
+		sys.exit(2)
+
+	for opt, arg in opts:
+
+		if opt in ("-h","--help"):
+			help()
+
+		elif opt == "--portinfo":
+			portinfo()
+
+		elif opt in ("-p","--port"):
+			openserialport(arg)
+			portopt = True
+
+		elif opt in ("-i","--info"):
+			if portopt == True:
+				info()
+			else:
+				print("no port was given")
+				help()
+
+		elif opt in ("-s","--set_value"):
+			if portopt == True:
+				setvalue(arg)
+			else:
+				print("no port was given")
+				help()
+			return
+
+		elif opt in ("-t","--csv_table"):
+			if portopt == True:
+				csvloop()
+			else:
+				print("no port was given")
+				help()
+			return
+
+		else:
+			print("no options given, try -h or --help")
+
+
+argumentcheck()
+
+sys.exit(2)
+
+
+#anstatt Benutztereingaben => argumente bei Skriptaufruf
+#zusätzliche Funktionen, help, einen Wert fest setzten, info aufrufen
+#Zeit als ersten Wert einlesen (erste Spalte in CSV) und  Zeit in Millisekunden parsen
+
+#was tun mit den Errors?
+#Fehleingaben abfangen
